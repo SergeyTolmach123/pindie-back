733d6dd2b381d199c0140e9c9493ce7e9dc7323c
SergeyTolmach123 <sergeytolmachyp@gmail.com>
2024-05-27 19:15:00 +0300

    work
    Проснитесь и пойте, мистер Фримен… Проснитесь и пойте… Нет я не хочу сказать, что вы спите на работе…
    
    Никто не заслуживает отдыха больше вашего. И все усилия мира пропали бы даром, пока…
    
    Эмм… Скажем просто, что ваш час снова пробил. Нужный человек не в том месте может перевернуть мир.
    
    Так проснитесь же, мистер Фримен… Проснитесь, вас снова ждут великие дела.

---
+++ olimpiada-okruzhayushiy-mir-2-klass/readme.md
@@ -0,0 +1,5 @@
+Ссылка на репозиторий с фронтендом: https://github.com/ebobotdll/pobediteli-olimpiadi-okruzhayushiy-mir-2-klass.git
+
+Ссылка на домен фронтенда: https://kerilloxfront.nomoredomainswork.ru/
+
+IP-адрес: 158.160.167.65
---
+++ olimpiada-okruzhayushiy-mir-2-klass/routes/games.js
@@ -0,0 +1,18 @@
+const gamesRouter = require('express').Router();
+
+const { findAllGames, createGame, findGameById, updateGame, deleteGame, checkEmptyFields, checkIfUsersAreSafe, checkIfCategoriesAvaliable, checkIsGameExists } = require('../middlewares/games');
+const { sendAllGames, sendGameById, sendGameUpdated, sendGameDeleted } = require('../controllers/games');
+const { checkAuth } = require("../middlewares/auth.js");
+
+const sendGameCreated = (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(req.game));
+  };
+
+gamesRouter.get('/games', findAllGames, sendAllGames);
+gamesRouter.get('/games/:id', findGameById, sendGameById);
+gamesRouter.post("/games", findAllGames, checkIsGameExists, checkIfCategoriesAvaliable, checkEmptyFields, checkAuth, createGame, sendGameCreated);
+gamesRouter.put("/games/:id", findGameById, checkIfUsersAreSafe, checkIfCategoriesAvaliable, checkEmptyFields, checkAuth,  updateGame, sendGameUpdated);
+gamesRouter.delete('/games/:id', checkAuth, deleteGame, sendGameDeleted);
+
+module.exports = gamesRouter;
